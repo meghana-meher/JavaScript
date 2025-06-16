fc77f58ffe3c3f4bb74881ea38cf5ea08a726a99
meghana-meher <meghanameher.tenneti@gmail.com>
2025-06-16 14:47:00 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,70 @@
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.clear()
+
+
+
+console.warn("This is a warning")
+console.table({name : "Meghana, age:82"})
+console.error("this is error")
+// clg -> console.log
+
+
+// variables
+
+// declared variables
+let name;
+// assign value
+let banana;
+banana="banannaanaa"
+console.log(banana)
+
+// arithematic operations
+let firstFavNum =5
+let secondFavNum =9
+
+console.log(firstFavNum+secondFavNum)
+console.log(firstFavNum-secondFavNum)
+console.log(firstFavNum*secondFavNum)
+console.log(firstFavNum/secondFavNum)
+console.log(firstFavNum%secondFavNum)
+console.log(firstFavNum**secondFavNum)
+
+// boolean
+// falsy values(u will get false)
+
+// false, null(programmer), undefined(system), 0, -0, NaN, '', "". ``,(empty quotes)
+
+let isJsProgrammingLanguage =true
+
+let isJsHard = false
+let favNum = 5
+console.log(isJsProgrammingLanguage)
+console.log(isJsHard)
+console.log(favNum + undefined) //NaN
+
+// comparison operators
+// relational operators
+console.log(10>10)//false
+console.log(10<10)//false
+console.log(10>=10)//true
+console.log(10<=10)//true
+
+// ===============================
+// equality operators
+// === strict (type + value)
+// !== strict non-equality operators(type + value)
+// == lose equality operator (value)
+// != lose not equality operator (value)
+
+console.log(10===10)//true
+console.log(10===10)//true
+console.log(10 == '10') //true
+console.log(10!=10)//false
+console.log(10!=20)//true
+
+
+
+
+
